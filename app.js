5394f12f13e39aad1ed56560e2366e7223b26e9a
Ó Conchubhair <ste@thewhitefox.ninja>
2018-04-30 16:44:37 +0100

    :tram: getElementById querySelector :japan:

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-let val;
+// document.getElementById()
 
-val = document;
-val = document.all;
-val = document.all[1];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+console.log(document.getElementById('task-title'));
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+// get stuff from the element
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
 
-let scripts = document.scripts;
+const taskTitle = document.getElementById('task-title');
 
-let scriptsArr = Array.from(scripts);
+// change styling
+taskTitle.style.background = '#333';
+taskTitle.style.color = '#fff';
+taskTitle.style.padding = '5px';
+// document.getElementById('task-title').style.display = 'none';
 
-scriptsArr.forEach(script => {
-  console.log(script.getAttribute('src'));
-});
+// change content
+taskTitle.textContent = 'Task List';
+taskTitle.innerText = 'My Tasks';
+taskTitle.innerHTML = '<span style="color:red">Task List</span>';
 
-for (const iterator of scriptsArr) {
-  console.log(iterator);
-}
+// document.querySelector()
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
 
-console.log(val);
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'yellow';
+document.querySelector('li:nth-child(4)').textContent = 'Hi there';
+document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
